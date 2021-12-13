6f3c37a986ddd0aa86a20c1acb4f2fdeb37fad8c
maxim <maxim@gritsenko.biz>
2021-12-13 08:26:24 +0300

    Adds get_agent method

---
+++ src/utils.rs
@@ -0,0 +1,42 @@
+use std::env;
+use std::path::PathBuf;
+use ic_agent::identity::BasicIdentity;
+use ic_agent::Agent;
+use ic_agent::agent::http_transport::ReqwestHttpReplicaV2Transport;
+
+fn get_identity_path(account_name: &str) -> PathBuf {
+    let home_folder = env::var("HOME").unwrap();
+    let mut path = PathBuf::new();
+    path.push(home_folder);
+    path.push(".config/dfx/identity");
+    path.push(account_name);
+    path.push("identity.pem");
+
+    path
+}
+
+pub fn get_identity(account_name: &str) -> BasicIdentity {
+    let identity = BasicIdentity::from_pem_file(get_identity_path(account_name)).unwrap();
+    identity
+}
+
+pub async fn get_agent(name: &str, url: &str) -> Agent {
+    let identity = get_identity(name);
+
+    let t: ReqwestHttpReplicaV2Transport =
+        ReqwestHttpReplicaV2Transport::create(url).unwrap();
+    let agent = Agent::builder()
+        .with_transport(t)
+        .with_identity(identity)
+        .build()
+        .expect("should work");
+
+    match agent.fetch_root_key().await {
+        Ok(_) => {}
+        Err(e) => {
+            println!("[get_agent] Error: {:?}", e)
+        }
+    }
+
+    agent
+}
