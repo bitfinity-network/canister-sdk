e090660bf1e2d363d757580e6ec153bc49cd8938
d23bf995 <ce791b50>
2021-12-15 13:20:26 +0100

    removed a function only used in one place and added the beginning of docs

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,5 @@
-use std::path::{Path, PathBuf};
+//! Utilities for testing
+use std::path::Path;
 
 use ic_agent::agent::http_transport::ReqwestHttpReplicaV2Transport;
 use ic_agent::identity::BasicIdentity;
@@ -6,21 +7,27 @@ use ic_agent::Agent;
 
 use crate::Result;
 
-fn get_identity_path(account_name: impl AsRef<Path>) -> Result<PathBuf> {
-    let mut path = dirs::config_dir().ok_or(crate::Error::MissingConfig)?;
-    path.push("dfx/identity");
-    path.push(account_name);
-    path.push("identity.pem");
-    Ok(path)
-}
+/// Get the identity for an account.
+/// This is useful for testing.
+pub fn get_identity(account_name: impl AsRef<Path>) -> Result<BasicIdentity> {
+    let mut ident_path = dirs::config_dir().ok_or(crate::Error::MissingConfig)?;
+    ident_path.push("dfx/identity");
+    ident_path.push(account_name);
+    ident_path.push("identity.pem");
 
-pub fn get_identity(account_name: impl AsRef<str>) -> Result<BasicIdentity> {
-    let ident_path = get_identity_path(account_name.as_ref())?;
     let identity = BasicIdentity::from_pem_file(ident_path)?;
     Ok(identity)
 }
 
-pub async fn get_agent(name: impl AsRef<str>, url: impl Into<String>) -> Result<Agent> {
+/// Get an agent by name.
+/// This is assuming there is an agent identity available.
+///
+/// ```text
+/// # Clone the identity project first
+/// mkdir -p ~/.config/dfx/identity/
+/// cp -Rn ./identity/.config/dfx/identity/* ~/.config/dfx/identity/
+/// ```
+pub async fn get_agent(name: impl AsRef<Path>, url: impl Into<String>) -> Result<Agent> {
     let identity = get_identity(name)?;
 
     let transport = ReqwestHttpReplicaV2Transport::create(url)?;
