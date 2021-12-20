bbadf7af3fcd5dc8ad9444ce22ae5a6571bf15ac
Navid <yaa110@gmail.com>
2021-12-20 17:47:33 +0330

    CPROD-300 add canister manager

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,18 @@
+#[cfg(not(target_arch = "wasm32"))]
 pub mod utils;
+
+#[cfg(target_arch = "wasm32")]
+pub mod state {
+    mod canister_manager;
+
+    pub use self::canister_manager::*;
+}
+
+#[cfg(target_arch = "wasm32")]
+pub mod types {
+    mod canister;
+    mod checksum;
+
+    pub use self::canister::*;
+    pub use self::checksum::*;
+}
