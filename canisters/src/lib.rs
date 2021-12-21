81430acfe89f4e51091ce2cb4252503ea968d8ec
Navid <yaa110@gmail.com>
2021-12-21 23:41:01 +0330

    Change the layout of project

--- canisters/src/lib.rs
+++ canisters/src/lib.rs
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-mod errors;
-pub mod utils;
-
-pub use errors::{Error, Result};
-=======
-#[cfg(not(target_arch = "wasm32"))]
-pub mod utils;
-
-#[cfg(target_arch = "wasm32")]
 pub mod factory {
     mod api;
     mod core;
@@ -24,19 +14,13 @@ pub mod factory {
     pub use self::core::*;
     pub use self::state::*;
 }
-<<<<<<< HEAD
->>>>>>> 23876d9 (CPROD-300 add canister factory)
-=======
 
-#[cfg(target_arch = "wasm32")]
 pub mod management {
     mod canister;
     pub use self::canister::*;
 }
 
-#[cfg(target_arch = "wasm32")]
 pub mod is20 {
     mod principal_ext;
     pub use self::principal_ext::*;
 }
->>>>>>> 41c43a7 (changes)
