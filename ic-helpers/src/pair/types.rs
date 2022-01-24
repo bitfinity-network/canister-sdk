1c3c85b7d01cf1a0d310a8b1b9180bca0e7e81fb
Maxim <maxim@gritsenko.biz>
2022-01-24 17:32:41 +0300

    Adds WeightsConfig type for configuring weights in AMM pool

--- ic-helpers/src/pair/types.rs
+++ ic-helpers/src/pair/types.rs
@@ -43,3 +43,21 @@ impl Default for TokenInfo {
         Self::empty()
     }
 }
+
+/// Configuration of token weights in AMM pool. This is used to configure weights on pool creation.
+#[derive(Debug, CandidType, Deserialize)]
+pub struct WeightsConfig {
+    pub weight0: f64,
+    pub weight1: f64,
+    pub change_allowed: bool,
+}
+
+impl Default for WeightsConfig {
+    fn default() -> Self {
+        Self {
+            weight0: 0.5,
+            weight1: 0.5,
+            change_allowed: false
+        }
+    }
+}
