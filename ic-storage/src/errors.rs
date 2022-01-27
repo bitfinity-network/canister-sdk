7de12d54089c3d4ebec5f2a32e51570220c61880
Hagsteel <me@hagsteel.com>
2022-01-27 16:03:13 +0100

    rustfmt

--- ic-storage/src/errors.rs
+++ ic-storage/src/errors.rs
@@ -1,16 +1,18 @@
-use thiserror::Error;
 use ic_cdk::api::stable::StableMemoryError;
+use thiserror::Error;
 pub type Result<T> = std::result::Result<T, Error>;
 
 #[derive(Debug, Error)]
 pub enum Error {
     #[error("Insufficient space available")]
     InsufficientSpace,
-    
+
     #[error("Stable memory error")]
     StableMemory,
 
-    #[error("Attempted downgrade, or trying to load a version older than what is currently stored")]
+    #[error(
+        "Attempted downgrade, or trying to load a version older than what is currently stored"
+    )]
     AttemptedDowngrade,
 
     #[error("Candid error: {0}")]
