b87e22f60a14d4b2295de8c203ee4f94dbc85017
Maxim <maxim@gritsenko.biz>
2022-03-07 12:36:38 +0300

    Adds `ic-canister` crate
    See the crate docs for the details

---
+++ build.sh
@@ -0,0 +1,7 @@
+set -e
+
+cargo build -p canister_a --target wasm32-unknown-unknown --release
+cargo build -p canister_b --target wasm32-unknown-unknown --release
+
+ic-cdk-optimizer target/wasm32-unknown-unknown/release/canister_a.wasm -o target/wasm32-unknown-unknown/release/canister_a-opt.wasm
+ic-cdk-optimizer target/wasm32-unknown-unknown/release/canister_b.wasm -o target/wasm32-unknown-unknown/release/canister_b-opt.wasm
