7e4df9cc1ba0dfc3f57459db0249f4918ecc8ebf
awkure <awkure@protonmail.ch>
2022-04-18 11:30:29 +0300

    Remove impl_factory_state_management macro

--- ic-helpers/src/factory/state.rs
+++ ic-helpers/src/factory/state.rs
@@ -8,56 +8,6 @@ use std::future::Future;
 use std::hash::Hash;
 use std::pin::Pin;
 
-/// This macro adds the following methods to the `$state` struct:
-/// * `stable_save` - used to save the state to the stable storage
-/// * `stable_restore` - used to load the state from the stable storage
-/// * `reset` - used to replace the state in in-memory storage with the current one. This method
-///   can be used in `init` method to set up the state.
-///
-/// It also provides `pre_upgrade` and `post_upgrade` functions.
-///
-/// IMPORTANT: This macro assumes that ths `$state` object is the only state used in the canister.
-/// If this is not true, than this implementation cannot be used for state stable storage.
-#[macro_export]
-macro_rules! impl_factory_state_management {
-    ( $state:ident, $bytecode:expr ) => {
-        impl $state {
-            pub fn stable_save(&self) -> Result<(), ::ic_helpers::factory::error::FactoryError> {
-                ::ic_kit::ic::stable_store((self,)).map_err(|e| {
-                    use ::ic_helpers::factory::error::FactoryError;
-                    FactoryError::StableStorageError(format!("{}", e))
-                })
-            }
-
-            pub fn stable_restore() -> Result<(), ::ic_helpers::factory::error::FactoryError> {
-                let (mut loaded,): (Self,) = ::ic_kit::ic::stable_restore().map_err(|e| {
-                    use ::ic_helpers::factory::error::FactoryError;
-                    FactoryError::StableStorageError(format!("{}", e))
-                })?;
-                loaded.factory.restore($bytecode);
-                loaded.reset();
-                Ok(())
-            }
-
-            pub fn reset(self) {
-                let state = State::get();
-                let mut state = state.borrow_mut();
-                *state = self;
-            }
-        }
-
-        #[::ic_cdk_macros::pre_upgrade]
-        fn pre_upgrade() {
-            $state::get().borrow().stable_save();
-        }
-
-        #[::ic_cdk_macros::post_upgrade]
-        fn post_upgrade() {
-            $state::stable_restore();
-        }
-    };
-}
-
 #[derive(Debug, CandidType, Deserialize)]
 pub struct FactoryConfiguration {
     ledger_principal: Principal,
