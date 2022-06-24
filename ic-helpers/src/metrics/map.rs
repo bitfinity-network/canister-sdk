aa7229de13216764c316b83728ea3bc34c64266d
awkure <awkure@protonmail.ch>
2022-06-24 16:15:01 +0300

    Interval changes

--- ic-helpers/src/metrics/map.rs
+++ ic-helpers/src/metrics/map.rs
@@ -49,41 +49,62 @@ pub trait Metrics: Canister {
         });
     }
 
-    fn set_interval(hours: u64) {
-        MetricsStorage::get().borrow_mut().metrics.interval_hours = hours;
+    fn set_interval(interval: Interval) {
+        MetricsStorage::get().borrow_mut().metrics.interval = interval;
+    }
+}
+
+#[derive(Debug, Copy, Clone, CandidType, Deserialize)]
+pub enum Interval {
+    PerMinute,
+    PerHour,
+    PerDay,
+    PerWeek,
+    Period { seconds: u64 },
+}
+
+impl Interval {
+    pub fn nanos(&self) -> u64 {
+        match self {
+            Interval::Period { seconds } => *seconds * 1e+9 as u64,
+            Interval::PerMinute => 60 * 1e+9 as u64,
+            Interval::PerHour => 60 * 60 * 1e+9 as u64,
+            Interval::PerDay => 24 * 60 * 60 * 1e+9 as u64,
+            Interval::PerWeek => 7 * 24 * 60 * 60 * 1e+9 as u64,
+        }
     }
 }
 
 #[derive(Clone, CandidType, Deserialize, Debug)]
 pub struct MetricsMap<T: IcStorage> {
-    interval_hours: u64,
+    interval: Interval,
     pub map: std::collections::BTreeMap<u64, T>,
 }
 
 impl<T: IcStorage> MetricsMap<T> {
-    pub fn new<const INTERVAL: u64>() -> Self {
+    pub fn new(interval: Interval) -> Self {
         Self {
-            interval_hours: INTERVAL,
+            interval,
             map: std::collections::BTreeMap::new(),
         }
     }
 
-    pub fn get_interval(&self) -> u64 {
-        self.interval_hours
+    pub fn get_interval(&self) -> Interval {
+        self.interval
     }
 
     pub fn insert(&mut self, new_metric: T) -> Option<T> {
-        let current_ts = ic_kit::ic::time() / (6u64.pow(10) * 60);
+        let current_ts = ic_kit::ic::time();
         let last_ts = self
             .map
             .iter()
             .next_back()
             .map(|(k, _)| *k)
             .unwrap_or(current_ts);
-        let new_ts = if current_ts < last_ts + self.interval_hours {
+        let new_ts = if current_ts < last_ts + self.interval.nanos() {
             last_ts
         } else {
-            current_ts - (current_ts % self.interval_hours)
+            current_ts - (current_ts % self.interval.nanos())
         };
         self.map.insert(new_ts, new_metric)
     }
@@ -91,6 +112,6 @@ impl<T: IcStorage> MetricsMap<T> {
 
 impl<T: IcStorage> std::default::Default for MetricsMap<T> {
     fn default() -> Self {
-        Self::new::<1>()
+        Self::new(Interval::PerHour)
     }
 }
