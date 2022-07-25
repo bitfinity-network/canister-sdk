39b60ae2327ef32e18e03056172b0b73d096f937
Yasir Shariff <goodwonder5@gmail.com>
2022-07-25 14:57:17 +0300

    added icrc ledger types and bumped up ic branches

--- ic-helpers/src/ledger/mod.rs
+++ ic-helpers/src/ledger/mod.rs
@@ -3,8 +3,12 @@ pub use principal_ext::*;
 
 pub use ledger_canister::{
     AccountIdBlob, AccountIdentifier, BinaryAccountBalanceArgs, BlockHeight,
-    LedgerCanisterInitPayload, Memo, SendArgs, Subaccount, TimeStamp, Tokens, TransferArgs,
-    TransferError, DEFAULT_TRANSFER_FEE,
+    LedgerCanisterInitPayload, Memo, SendArgs, Subaccount, Tokens, TransferArgs, TransferError,
+    DEFAULT_TRANSFER_FEE,
 };
+
+pub use ic_icrc1::{Account, Subaccount as ICRCSubaccount};
+
+pub use ic_icrc1_ledger::{InitArgs as ICRCInitArgs, Ledger};
 
 pub use ic_base_types::PrincipalId;
