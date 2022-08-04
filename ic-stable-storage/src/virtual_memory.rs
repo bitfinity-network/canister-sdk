23a71096adf03476bff0baecf318b233b064252e
Hagsteel <me@hagsteel.com>
2022-08-04 16:33:03 +0200

    comment cleanup

--- ic-stable-storage/src/virtual_memory.rs
+++ ic-stable-storage/src/virtual_memory.rs
@@ -1,83 +1,10 @@
 use super::{Pages, WASM_PAGE_SIZE};
 use crate::Memory;
 
-// fn get_page_memory() -> StableMemory {
-//     PAGE_MEMORY.with(|memory| {
-//         let mem: &mut Option<StableMemory> = &mut memory.borrow_mut();
-//         match mem {
-//             Some(mem) => mem.clone(),
-//             None => {
-//                 let stable_memory = StableMemory::default();
-//                 *mem = Some(stable_memory.clone());
-//                 stable_memory
-//             }
-//         }
-//     })
-// }
-
-// struct PageMemory(StableBTreeMap<StableMemory, Vec<u8>, u8>);
-
-// impl PageMemory {
-//     fn instance() -> Self {
-//         panic!()
-//         // let memory = get_page_memory();
-//         // let map = StableBTreeMap::load(memory, MAX_PAGE_MEM_KEY_SIZE, MAX_PAGE_MEM_VALUE_SIZE);
-//         // Self(RefCell::new(map))
-//     }
-
-//     fn reload(&mut self) {
-//         // let mem = self.0.borrow_mut().get_memory();
-//         // self.0 = StableBTreeMap::load(mem);
-//     }
-
-//     fn free_page(&mut self, page_index: Vec<u8>) {
-//         let storage = &mut self.0;
-//         storage.remove(&page_index);
-//         let mut key = page_index;
-//         key[0] = u8::MAX;
-//         storage
-//             .insert(key, 0)
-//             .expect("insert pages to manager err");
-//     }
-// }
-
-// /// Manger is used to manage VirtualMemory. The specific function i&s to mark which wasm page in
-// /// memory belongs to which data, for example, the 0th page belongs to Balance, the 1st page belongs to History, etc.
-// pub struct Pages {
-//     memory: PageMemory,
-//     page_index: u8,
-// }
-
-// impl Pages {
-//     fn new(page_index: u8) -> Self {
-//         Self {
-//             memory: PageMemory::instance(),
-//             page_index,
-//         }
-//     }
-
-//     fn reload(&mut self) {
-//         self.memory.reload();
-//     }
-
-//     // fn range(&self) -> impl Iterator<Item = (Vec<u8>, u8)> + '_ {
-//     //     self.memory.0.borrow().range(vec![self.page_index], None)
-//     // }
-
-//     // fn free_pages(&self) -> impl Iterator<Item = (Vec<u8>, u8)> + '_ {
-//     //     self.memory.0.range(vec![u8::MAX], None)
-//     // }
-
-//     // fn free_page(&mut self, page_index: Vec<u8>) {
-//     //     self.memory.free_page(page_index)
-//     // }
-// }
-
 /// Pack fragmented memory composed of different pages into contiguous memory.
 ///
 /// index stand for different data structures.
 /// In the same canister, different data structures should use different indexes.
-// #[derive(Clone)]
 pub struct VirtualMemory<M1: Memory, const INDEX: u8> {
     memory: M1,
     pages: Pages,
