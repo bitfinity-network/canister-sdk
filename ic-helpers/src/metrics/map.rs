d3cd7d6b0dd5d4b0ffbdf56a2fa6cd2075f45b39
Maxim <maxim@gritsenko.biz>
2022-09-22 19:53:40 +0300

    Add `PartialEq` to `MetricsData`

--- ic-helpers/src/metrics/map.rs
+++ ic-helpers/src/metrics/map.rs
@@ -9,7 +9,7 @@ pub struct MetricsStorage {
     pub metrics: MetricsMap<MetricsData>,
 }
 
-#[derive(CandidType, Deserialize, IcStorage, Default, Clone, Debug)]
+#[derive(CandidType, Deserialize, IcStorage, Default, Clone, Debug, PartialEq)]
 pub struct MetricsData {
     pub cycles: u64,
     pub stable_memory_size: u64,
