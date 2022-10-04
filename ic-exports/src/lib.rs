dac88a20ea5097c60db2a9a6ca889607a94ee02e
Yasir Shariff <goodwonder5@gmail.com>
2022-10-04 13:46:37 +0300

    CPROD-1208 add icrc1 index crate types

--- ic-exports/src/lib.rs
+++ ic-exports/src/lib.rs
@@ -5,6 +5,10 @@ pub use ic_cdk_macros;
 pub use cycles_minting_canister;
 pub use ic_base_types;
 pub use ic_ic00_types;
+pub use ic_icrc1_index::{
+    GetAccountTransactionsArgs, GetTransactions, GetTransactionsErr, GetTransactionsResult,
+    TransactionWithId,
+};
 pub use ic_ledger_core;
 pub use ledger_canister;
 pub use stable_structures;
