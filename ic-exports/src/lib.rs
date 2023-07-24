de9570e9a76cf1182922fbf6d6ead79b31bcdecf
Francesco_Cina <ufoscout@gmail.com>
2023-07-24 15:14:49 +0200

    modularize exports

--- ic-exports/src/lib.rs
+++ ic-exports/src/lib.rs
@@ -1,22 +1,31 @@
 pub use candid; // this is needed for candid-derive macro exports
 pub use ic_cdk::export::*;
 pub use {
-    cycles_minting_canister, ic_base_types, ic_cdk, ic_cdk_macros, ic_cdk_timers, ic_crypto_sha,
-    ic_ic00_types, ic_icrc1, ic_icrc1_index, ic_icrc1_ledger as icrc1_ledger, ic_kit,
-    ic_ledger_canister_core, ic_ledger_core, ic_stable_structures as stable_structures,
-    icrc_ledger_types as icrc_types, ledger_canister,
+    ic_cdk, ic_cdk_macros, ic_cdk_timers, 
+    ic_kit,
+    ic_stable_structures as stable_structures,
+};
+
+pub use {
+    cycles_minting_canister, ic_base_types, ic_crypto_sha,
+    ic_ic00_types, ic_icrc1, ic_icrc1_index
 };
 
 pub type BlockHeight = u64;
 
-pub mod ledger {
-    pub use ic_ledger_core::Tokens;
-    pub use icp_ledger::{
-        AccountIdentifier, BinaryAccountBalanceArgs, CandidOperation, GetBlocksArgs,
-        LedgerCanisterInitPayload, QueryBlocksResponse, Subaccount, TransferArgs, TransferError,
-        TransferFee, TransferFeeArgs, DEFAULT_TRANSFER_FEE, TOKEN_SUBDIVIDABLE_BY,
-    };
-}
+// pub mod ledger {
+//     pub use ic_ledger_core::Tokens;
+//     pub use icp_ledger::{
+//         AccountIdentifier, BinaryAccountBalanceArgs, CandidOperation, GetBlocksArgs,
+//         LedgerCanisterInitPayload, QueryBlocksResponse, Subaccount, TransferArgs, TransferError,
+//         TransferFee, TransferFeeArgs, DEFAULT_TRANSFER_FEE, TOKEN_SUBDIVIDABLE_BY,
+//     };
+// }
+// pub use {
+//     ic_icrc1_ledger as icrc1_ledger,
+//     ic_ledger_canister_core, ic_ledger_core,
+//     icrc_ledger_types as icrc_types, ledger_canister,
+// };
 
 #[cfg(feature = "state-machine")]
 pub use ic_state_machine_tests;
