877313aba9441fbc9fae9cf79185a30eff3a2f91
veeso <christian.visintin@veeso.dev>
2023-08-08 17:38:18 +0200

    test

--- ic-stable-structures/src/storage_wasm/structures_wasm.rs
+++ ic-stable-structures/src/storage_wasm/structures_wasm.rs
@@ -1,12 +1,10 @@
 use ic_exports::stable_structures::memory_manager::MemoryId;
-use ic_exports::stable_structures::{
-    btreemap, cell, log, ring_buffer, vec, BoundedStorable, Storable,
-};
+use ic_exports::stable_structures::{btreemap, cell, log, vec, BoundedStorable, Storable};
 
 use crate::unbounded::{self, SlicedStorable};
 use crate::{get_memory_by_id, multimap, Error, Iter, Memory, RangeIter, Result};
 
-pub use ic_exports::stable_structures::ring_buffer::{Indices, StableRingBuffer};
+pub use ic_exports::stable_structures::{Indices as StableRingBufferIndices, StableRingBuffer};
 
 /// Stores value in stable memory, providing `get()/set()` API.
 pub struct StableCell<T: Storable>(cell::Cell<T, Memory>);
