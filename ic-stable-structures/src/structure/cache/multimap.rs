648d3c3b8a12cb3a11cb6146f7812f6e6e35988e
Francesco_Cina <ufoscout@gmail.com>
2023-09-01 16:36:22 +0200

    remp all cached structures

--- ic-stable-structures/src/structure/cache/multimap.rs
+++ ic-stable-structures/src/structure/cache/multimap.rs
@@ -1,8 +1,8 @@
 use std::cell::RefCell;
-use std::collections::VecDeque;
 use std::hash::Hash;
 
 use ic_exports::stable_structures::{memory_manager::MemoryId, BoundedStorable};
+use mini_moka::unsync::{Cache, CacheBuilder};
 
 use crate::structure::*;
 
@@ -14,18 +14,7 @@ where
     V: BoundedStorable + Clone,
 {
     inner: StableMultimap<K1, K2, V>,
-    cache: RefCell<Cache<K1, K2, V>>,
-}
-
-struct Cache<K1, K2, V>
-where
-    K1: BoundedStorable + Clone + Hash + Eq + PartialEq + Ord,
-    K2: BoundedStorable + Clone + Hash + Eq + PartialEq + Ord,
-    V: BoundedStorable + Clone,
-{
-    cache: heap::HeapMultimap<K1, K2, V>,
-    cache_keys: VecDeque<(K1, K2)>,
-    cache_max_items: usize,
+    cache: RefCell<Cache<(K1, K2), V>>,
 }
 
 impl<K1, K2, V> CachedStableMultimap<K1, K2, V>
@@ -35,41 +24,16 @@ where
     V: BoundedStorable + Clone,
 {
     /// Create new instance of the CachedStableMultimap with a fixed number of max cached elements.
-    pub fn new(memory_id: MemoryId, max_cache_items: usize) -> Self {
+    pub fn new(memory_id: MemoryId, max_cache_items: u64) -> Self {
         Self::with_map(StableMultimap::new(memory_id), max_cache_items)
     }
 
     /// Create new instance of the CachedStableMultimap with a fixed number of max cached elements.
-    pub fn with_map(inner: StableMultimap<K1, K2, V>, cache_max_items: usize) -> Self {
+    pub fn with_map(inner: StableMultimap<K1, K2, V>, max_cache_items: u64) -> Self {
         Self {
             inner,
-            cache: RefCell::new(Cache {
-                cache_max_items,
-                cache: Default::default(),
-                cache_keys: Default::default(),
-            }),
-        }
-    }
+            cache: RefCell::new(CacheBuilder::default().max_capacity(max_cache_items).build()),
 
-    #[inline]
-    fn remove_oldest_from_cache(&self, cache: &mut Cache<K1, K2, V>) {
-        if cache.cache_keys.len() > cache.cache_max_items {
-            if let Some((k1, k2)) = cache.cache_keys.pop_front() {
-                cache.cache.remove(&k1, &k2);
-            };
-        }
-    }
-
-    #[inline]
-    fn remove_from_cache_by_keys(&self, first_key: &K1, second_key: &K2, cache: &mut Cache<K1, K2, V>) {
-        if cache.cache.remove(first_key, second_key).is_some() {
-            if let Some(pos) = cache
-                .cache_keys
-                .iter()
-                .position(|(k1, k2)| k1 == first_key && k2 == second_key)
-            {
-                cache.cache_keys.remove(pos);
-            }
         }
     }
 
@@ -81,32 +45,17 @@ where
     K2: BoundedStorable + Clone + Hash + Eq + PartialEq + Ord,
     V: BoundedStorable + Clone,
 {
-    fn insert(&mut self, first_key: &K1, second_key: &K2, value: &V) -> Option<V> {
-        match self.inner.insert(first_key, second_key, value) {
-            Some(old_value) => {
-                self.remove_from_cache_by_keys(first_key, second_key, &mut self.cache.borrow_mut());
-                Some(old_value)
-            },
-            None => None,
-        }
-    }
 
     fn get(&self, first_key: &K1, second_key: &K2) -> Option<V> {
-        let cache = self.cache.borrow();
-        match cache.cache.get(first_key, second_key) {
-            Some(value) => Some(value),
+        let mut cache = self.cache.borrow_mut();
+        let key = (first_key.clone(), second_key.clone());
+
+        match cache.get(&key) {
+            Some(value) => Some(value.clone()),
             None => {
-                drop(cache);
                 match self.inner.get(first_key, second_key) {
                     Some(value) => {
-                        {
-                            let mut cache = self.cache.borrow_mut();
-                            cache.cache.insert(first_key, second_key, &value);
-                            cache
-                                .cache_keys
-                                .push_back((first_key.clone(), second_key.clone()));
-                            self.remove_oldest_from_cache(&mut cache);
-                        }
+                        cache.insert(key, value.clone());
                         Some(value)
                     }
                     None => None,
@@ -115,10 +64,22 @@ where
         }
     }
 
+    fn insert(&mut self, first_key: &K1, second_key: &K2, value: &V) -> Option<V> {
+        match self.inner.insert(first_key, second_key, value) {
+            Some(old_value) => {
+                let key = (first_key.clone(), second_key.clone());
+                self.cache.borrow_mut().invalidate(&key);
+                Some(old_value)
+            },
+            None => None,
+        }
+    }
+
     fn remove(&mut self, first_key: &K1, second_key: &K2) -> Option<V> {
         match self.inner.remove(first_key, second_key) {
             Some(old_value) => {
-                self.remove_from_cache_by_keys(first_key, second_key, &mut self.cache.borrow_mut());
+                let key = (first_key.clone(), second_key.clone());
+                self.cache.borrow_mut().invalidate(&key);
                 Some(old_value)
             },
             None => None,
@@ -126,12 +87,7 @@ where
     }
 
     fn remove_partial(&mut self, first_key: &K1) -> bool {
-        {
-            let mut cache = self.cache.borrow_mut();
-            if cache.cache.remove_partial(first_key) {
-                cache.cache_keys.retain(|(k1, _k2)| k1 != first_key);
-            }
-        }
+        self.cache.borrow_mut().invalidate_entries_if(|(k1, _k2), _v| k1 == first_key);
         self.inner.remove_partial(first_key)
     }
 
@@ -144,11 +100,7 @@ where
     }
 
     fn clear(&mut self) {
-        {
-            let mut cache = self.cache.borrow_mut();
-            cache.cache.clear();
-            cache.cache_keys.clear();
-        }
+        self.cache.borrow_mut().invalidate_all();
         self.inner.clear()
     }
 }
@@ -156,7 +108,7 @@ where
 #[cfg(test)]
 mod test {
 
-    use ic_exports::stable_structures::{memory_manager::MemoryId, Storable};
+    use ic_exports::stable_structures::memory_manager::MemoryId;
     use crate::test_utils::Array;
 
     use super::*;
@@ -198,111 +150,49 @@ mod test {
         assert_eq!(None, map.get(&3, &1));
     }
 
-    //     #[test]
-    //     fn inserts() {
-    //         let mut mm = CachedStableMultimap::new(DefaultMemoryImpl::default());
-    //         for i in 0..10 {
-    //             let k1 = Array([i; 1]);
-    //             let k2 = Array([i * 10; 2]);
-    //             let val = Array([i; 1]);
-    //             mm.insert(&k1, &k2, &val);
-    //         }
+    #[test]
+    fn should_clear() {
+        let cache_items = 2;
+        let mut map: CachedStableMultimap<u32, u32, Array<2>> =
+        CachedStableMultimap::<u32, u32, Array<2>>::new(MemoryId::new(101), cache_items);
 
-    //         assert_eq!(mm.len(), 10);
-    //     }
+        assert_eq!(None, map.insert(&1, &1, &Array([1u8, 1])));
+        assert_eq!(None, map.insert(&2, &1, &Array([2u8, 1])));
+        assert_eq!(None, map.insert(&3, &1, &Array([3u8, 1])));
 
-    //     #[test]
-    //     fn insert_should_replace_old_value() {
-    //         let mut mm = make_map();
+        assert_eq!(Some(Array([1u8, 1])), map.get(&1, &1));
+        assert_eq!(Some(Array([2u8, 1])), map.get(&2, &1));
 
-    //         let k1 = Array([1u8, 2]);
-    //         let k2 = Array([11u8, 12, 13]);
-    //         let val = Array([255u8, 255, 255, 255, 255, 255]);
+        map.clear();
+        
+        assert_eq!(0, map.len());
 
-    //         let prev_val = Array([200u8, 200, 200, 100, 100, 123]);
-    //         let replaced_val = mm.insert(&k1, &k2, &val).unwrap();
+        assert_eq!(None, map.get(&1, &1));
+        assert_eq!(None, map.get(&2, &1));
 
-    //         assert_eq!(prev_val, replaced_val);
-    //         assert_eq!(mm.get(&k1, &k2), Some(val));
-    //     }
+    }
 
-    //     #[test]
-    //     fn get() {
-    //         let mm = make_map();
-    //         let k1 = Array([1u8, 2]);
-    //         let k2 = Array([11u8, 12, 13]);
-    //         let val = mm.get(&k1, &k2).unwrap();
+    #[test]
+    fn should_replace_old_value() {
+        let cache_items = 2;
+        let mut map: CachedStableMultimap<u32, u32, Array<2>> =
+        CachedStableMultimap::<u32, u32, Array<2>>::new(MemoryId::new(102), cache_items);
 
-    //         let expected = Array([200u8, 200, 200, 100, 100, 123]);
-    //         assert_eq!(val, expected);
-    //     }
+        assert_eq!(None, map.insert(&1, &1, &Array([1u8, 1])));
+        assert_eq!(None, map.insert(&2, &1, &Array([2u8, 1])));
+        assert_eq!(None, map.insert(&3, &1, &Array([3u8, 1])));
+        assert_eq!(3, map.len());
 
-    //     #[test]
-    //     fn remove() {
-    //         let mut mm = make_map();
-    //         let k1 = Array([1u8, 2]);
-    //         let k2 = Array([11u8, 12, 13]);
-    //         let val = mm.remove(&k1, &k2).unwrap();
+        assert_eq!(Some(Array([1u8, 1])), map.get(&1, &1));
+        assert_eq!(Some(Array([2u8, 1])), map.get(&2, &1));
 
-    //         let expected = Array([200u8, 200, 200, 100, 100, 123]);
-    //         assert_eq!(val, expected);
-    //         assert_eq!(mm.len(), 1);
+        assert_eq!(Some(Array([1u8, 1])), map.insert(&1, &1, &Array([1u8, 10])));
+        assert_eq!(Some(Array([3u8, 1])), map.insert(&3, &1, &Array([3u8, 10])));
 
-    //         let k1 = Array([10u8, 20]);
-    //         let k2 = Array([21u8, 22, 23]);
-    //         mm.remove(&k1, &k2).unwrap();
-    //         assert!(mm.is_empty());
-    //     }
+        assert_eq!(Some(Array([1u8, 10])), map.get(&1, &1));
+        assert_eq!(Some(Array([2u8, 1])), map.get(&2, &1));
+        assert_eq!(Some(Array([3u8, 10])), map.get(&3, &1));
 
-    //     #[test]
-    //     fn remove_partial() {
-    //         let mut mm = CachedStableMultimap::new(DefaultMemoryImpl::default());
-    //         let k1 = Array([1u8, 2]);
-    //         let k2 = Array([11u8, 12, 13]);
-    //         let val = Array([200u8, 200, 200, 100, 100, 123]);
-    //         mm.insert(&k1, &k2, &val);
+    }
 
-    //         let k2 = Array([21u8, 22, 23]);
-    //         let val = Array([123, 200u8, 200, 100, 100, 255]);
-    //         mm.insert(&k1, &k2, &val);
-
-    //         mm.remove_partial(&k1);
-    //         assert!(mm.is_empty());
-    //     }
-
-    //     #[test]
-    //     fn clear() {
-    //         let mut mm = CachedStableMultimap::new(DefaultMemoryImpl::default());
-    //         let k1 = Array([1u8, 2]);
-    //         let k2 = Array([11u8, 12, 13]);
-    //         let val = Array([200u8, 200, 200, 100, 100, 123]);
-    //         mm.insert(&k1, &k2, &val);
-
-    //         let k2 = Array([21u8, 22, 23]);
-    //         let val = Array([123, 200u8, 200, 100, 100, 255]);
-    //         mm.insert(&k1, &k2, &val);
-    //         let k1 = Array([21u8, 22]);
-    //         mm.insert(&k1, &k2, &val);
-
-    //         mm.clear();
-    //         assert!(mm.is_empty());
-    //     }
-
-    //     #[test]
-    //     fn iter() {
-    //         let mm = make_map();
-    //         let mut iter = mm.into_iter();
-    //         assert!(iter.next().is_some());
-    //         assert!(iter.next().is_some());
-    //         assert!(iter.next().is_none());
-    //     }
-
-    //     #[test]
-    //     fn range_iter() {
-    //         let k1 = Array([1u8, 2]);
-    //         let mm = make_map();
-    //         let mut iter = mm.range(&k1);
-    //         assert!(iter.next().is_some());
-    //         assert!(iter.next().is_none());
-    //     }
 }
