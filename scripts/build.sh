97917a6cdf9346a21d660e492339df8e29b5ff5d
Dmytro Gordon <g-d@ukr.net>
2023-09-29 12:11:32 +0300

    Fix build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,14 +6,14 @@ cargo run -p canister-c --features export-api > ic-canister/tests/canister-c/can
 cargo run -p canister-d --features export-api > ic-canister/tests/canister-d/canister-d.did
 cargo run -p dummy_canister --features export-api > ic-stable-structures/tests/dummy_canister/dummy_canister.did
 
-cargo build -p canister-a --lib --target wasm32-unknown-unknown --features export-api --release
-cargo build -p canister-b --lib --target wasm32-unknown-unknown --features export-api --release
-cargo build -p canister-c --lib --target wasm32-unknown-unknown --features export-api --release
-cargo build -p canister-d --lib --target wasm32-unknown-unknown --features export-api --release
+cargo build -p canister-a --target wasm32-unknown-unknown --features export-api --release
+cargo build -p canister-b --target wasm32-unknown-unknown --features export-api --release
+cargo build -p canister-c --target wasm32-unknown-unknown --features export-api --release
+cargo build -p canister-d --target wasm32-unknown-unknown --features export-api --release
 cargo build -p dummy_canister --target wasm32-unknown-unknown --features export-api --release
 
-ic-wasm target/wasm32-unknown-unknown/release/canister_a.wasm -o target/wasm32-unknown-unknown/release/canister-a.wasm shrink
-ic-wasm target/wasm32-unknown-unknown/release/canister_b.wasm -o target/wasm32-unknown-unknown/release/canister-b.wasm shrink
-ic-wasm target/wasm32-unknown-unknown/release/canister_c.wasm -o target/wasm32-unknown-unknown/release/canister-c.wasm shrink
-ic-wasm target/wasm32-unknown-unknown/release/canister_d.wasm -o target/wasm32-unknown-unknown/release/canister-d.wasm shrink
+ic-wasm target/wasm32-unknown-unknown/release/canister-a.wasm -o target/wasm32-unknown-unknown/release/canister-a.wasm shrink
+ic-wasm target/wasm32-unknown-unknown/release/canister-b.wasm -o target/wasm32-unknown-unknown/release/canister-b.wasm shrink
+ic-wasm target/wasm32-unknown-unknown/release/canister-c.wasm -o target/wasm32-unknown-unknown/release/canister-c.wasm shrink
+ic-wasm target/wasm32-unknown-unknown/release/canister-d.wasm -o target/wasm32-unknown-unknown/release/canister-d.wasm shrink
 
