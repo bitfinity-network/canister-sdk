6e9d13fa332a9be399867dc8aa8a8f8aa6a8dbc6
Francesco_Cina <ufoscout@gmail.com>
2023-10-26 16:30:26 +0200

    default mm

--- ic-stable-structures/src/memory.rs
+++ ic-stable-structures/src/memory.rs
@@ -1,7 +1,7 @@
 use dfinity_stable_structures::memory_manager::{
     MemoryId, MemoryManager as IcMemoryManager, VirtualMemory,
 };
-use dfinity_stable_structures::Memory;
+use dfinity_stable_structures::{Memory, DefaultMemoryImpl};
 
 pub trait MemoryManager<M: Memory, T> {
     fn get(&self, id: T) -> M;
@@ -17,4 +17,9 @@ impl<M: Memory> MemoryManager<VirtualMemory<M>, MemoryId> for IcMemoryManager<M>
     fn get(&self, id: MemoryId) -> VirtualMemory<M> {
         self.get(id)
     }
+}
+
+/// Returns a new IC default MemoryManager
+pub fn default_ic_memory_manager() -> IcMemoryManager<DefaultMemoryImpl> {
+    IcMemoryManager::init(DefaultMemoryImpl::default())
 }
