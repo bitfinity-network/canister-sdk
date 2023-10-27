d63bd416138949b325f1f982d4d7042012f5e8b3
Dmytro Gordon <g-d@ukr.net>
2023-10-27 10:53:58 +0300

    Add save copy functionality

--- ic-stable-structures/src/memory_mapped_files/memory.rs
+++ ic-stable-structures/src/memory_mapped_files/memory.rs
@@ -1,9 +1,12 @@
+use std::collections::BTreeMap;
+use std::collections::btree_map::Entry;
 use std::path::{Path, PathBuf};
+use std::sync::Arc;
 
 use dfinity_stable_structures::Memory;
 use parking_lot::RwLock;
 
-use super::error::MemMapResult;
+use super::error::{MemMapResult, MemMapError};
 use super::memory_mapped_file::MemoryMappedFile;
 use crate::memory::MemoryManager;
 
@@ -12,6 +15,7 @@ const WASM_PAGE_SIZE_IN_BYTES: u64 = 65536;
 pub struct MemoryMappedFileMemoryManager {
     base_path: PathBuf,
     is_persistent: bool,
+    created_memory_resources: Arc<RwLock<BTreeMap<PathBuf, MemoryMappedFileMemory>>>,
 }
 
 impl MemoryMappedFileMemoryManager {
@@ -19,47 +23,72 @@ impl MemoryMappedFileMemoryManager {
         Self {
             base_path,
             is_persistent,
+            created_memory_resources: Default::default(),
+        }
+    }
+
+    pub fn flush_and_save_copies_to(&self, path: impl AsRef<Path>) -> Result<(), MemMapError> {
+        let created_memory_resources = self.created_memory_resources.read();
+        for (file_path, memory) in created_memory_resources.iter() {
+            let file_name = file_path.file_name().ok_or(MemMapError::InvalidSourceFileName)?;
+            let new_path = path.as_ref().join(file_name.to_str().ok_or(MemMapError::InvalidSourceFileName)?);
+
+            memory.save_copy(new_path)?;
+        }
+
+        Ok(())
+    }
+
+    fn get_impl(&self, id: impl AsRef<Path>) -> MemoryMappedFileMemory {
+        let mut created_memory_resources = self.created_memory_resources.write();
+        let file_path = self.base_path.join(id.as_ref());
+        match created_memory_resources.entry(file_path) {
+            Entry::Vacant(entry) => {
+                let file_path = entry.key()
+                    .to_str()
+                    .expect(&format!("Cannot extract path from {}", entry.key().display()));
+                let result = MemoryMappedFileMemory::new(file_path.to_owned(), self.is_persistent).expect(&format!(
+                    "failed to initialize MemoryMappedFileMemory with path: {}",
+                    file_path));
+
+                entry.insert(result.clone());
+
+                result
+            },
+            Entry::Occupied(entry) => {
+                entry.get().clone()
+            },
         }
     }
 }
 
 impl MemoryManager<MemoryMappedFileMemory, &str> for MemoryMappedFileMemoryManager {
     fn get(&self, id: &str) -> MemoryMappedFileMemory {
-        get(&self.base_path, self.is_persistent, id)
+        self.get_impl(id)
     }
 }
 
 impl MemoryManager<MemoryMappedFileMemory, &Path> for MemoryMappedFileMemoryManager {
     fn get(&self, id: &Path) -> MemoryMappedFileMemory {
-        get(&self.base_path, self.is_persistent, id)
+        self.get_impl(id)
     }
 }
 
 impl MemoryManager<MemoryMappedFileMemory, u8> for MemoryMappedFileMemoryManager {
     fn get(&self, id: u8) -> MemoryMappedFileMemory {
-        get(&self.base_path, self.is_persistent, id.to_string())
+        self.get_impl( id.to_string())
     }
 }
 
-fn get<T: AsRef<Path>>(base_path: &PathBuf, is_persistent: bool, id: T) -> MemoryMappedFileMemory {
-    let file_path = base_path.join(id.as_ref());
-    let file_path = file_path
-        .to_str()
-        .expect(&format!("Cannot extract path from {}", file_path.display()));
-    MemoryMappedFileMemory::new(file_path.to_owned(), is_persistent).expect(&format!(
-        "failed to initialize MemoryMappedFileMemory with path: {}",
-        file_path
-    ))
-}
-
-pub struct MemoryMappedFileMemory(RwLock<MemoryMappedFile>);
+#[derive(Clone)]
+pub struct MemoryMappedFileMemory(Arc<RwLock<MemoryMappedFile>>);
 
 impl MemoryMappedFileMemory {
     pub fn new(path: String, is_persistent: bool) -> MemMapResult<Self> {
-        Ok(Self(RwLock::new(MemoryMappedFile::new(
+        Ok(Self(Arc::new(RwLock::new(MemoryMappedFile::new(
             path,
             is_persistent,
-        )?)))
+        )?))))
     }
 
     pub fn set_is_persistent(&self, is_persistent: bool) {
