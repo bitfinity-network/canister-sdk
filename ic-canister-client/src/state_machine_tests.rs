9849df2011c25c270e1d1f9638417ec07fba7a18
Francesco_Cina <ufoscout@gmail.com>
2023-11-09 17:52:27 +0100

    duplicate some methods

--- ic-canister-client/src/state_machine_tests.rs
+++ ic-canister-client/src/state_machine_tests.rs
@@ -79,6 +79,62 @@ impl StateMachineCanisterClient {
         .await
         .unwrap()
     }
+
+    pub async fn update<T, R>(&self, method: &str, args: T) -> CanisterClientResult<R>
+    where
+        T: ArgumentEncoder + Send + Sync,
+        R: for<'de> Deserialize<'de> + CandidType,
+    {
+        let args = candid::encode_args(args)?;
+        let method = String::from(method);
+
+        let call_result = self
+            .with_state_machine(move |env, canister, caller| {
+                env.update_call(canister, caller, &method, args)
+            })
+            .await?;
+
+        let reply = match call_result {
+            WasmResult::Reply(reply) => reply,
+            WasmResult::Reject(e) => {
+                return Err(CanisterClientError::CanisterError((
+                    RejectionCode::CanisterError,
+                    e,
+                )));
+            }
+        };
+
+        let decoded = Decode!(&reply, R)?;
+        Ok(decoded)
+    }
+
+    pub async fn query<T, R>(&self, method: &str, args: T) -> CanisterClientResult<R>
+    where
+        T: ArgumentEncoder + Send + Sync,
+        R: for<'de> Deserialize<'de> + CandidType,
+    {
+        let args = candid::encode_args(args)?;
+        let method = String::from(method);
+
+        let call_result = self
+            .with_state_machine(move |env, canister, caller| {
+                env.query_call(canister, caller, &method, args)
+            })
+            .await?;
+
+        let reply = match call_result {
+            WasmResult::Reply(reply) => reply,
+            WasmResult::Reject(e) => {
+                return Err(CanisterClientError::CanisterError((
+                    RejectionCode::CanisterError,
+                    e,
+                )));
+            }
+        };
+
+        let decoded = Decode!(&reply, R)?;
+        Ok(decoded)
+    }
 }
 
 #[async_trait::async_trait]
