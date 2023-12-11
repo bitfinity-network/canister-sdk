05944d0d941f5090ad1f36b49160006d540c79e2
Francesco_Cina <ufoscout@gmail.com>
2023-12-11 17:20:48 +0100

    stub rework

--- ic-stable-structures/src/task/mod.rs
+++ ic-stable-structures/src/task/mod.rs
@@ -15,7 +15,7 @@ pub trait SyncTask {
 pub trait AsyncTask {
 
     /// Execute the task and return the next task to execute.
-    fn execute(&self) -> Pin<Box<dyn std::future::Future<Output = Option<Task>>>>;
+    fn execute(&self) -> Pin<Box<dyn std::future::Future<Output = Option<Task>> + Send>>;
 }
 
 /// A task is a unit of work that can be executed by the scheduler.
@@ -37,11 +37,11 @@ impl From<Box<dyn AsyncTask>> for Task {
 }
 
 /// A scheduler is responsible for executing tasks.
-pub struct Scheduler<T: VecStructure<Task>> {
+pub struct Scheduler<T: 'static + VecStructure<Task>> {
     pending_tasks: Arc<Mutex<T>>,
 }
 
-impl <T: VecStructure<Task>> Scheduler<T> {
+impl <T: 'static + VecStructure<Task>> Scheduler<T> {
 
     pub fn new(pending_tasks: T) -> Self {
         Self {
@@ -63,11 +63,11 @@ impl <T: VecStructure<Task>> Scheduler<T> {
     pub fn run(&mut self) -> Result<()> {
         while let Some(task) = self.pending_tasks.lock().get(0) {
             match task {
-                Task::Sync() => {
-                    println!("Sync task");
+                Task::Sync(task) => {
+                    execute_sync_task(task, self.pending_tasks.clone())
                 },
-                Task::Async() => {
-                    println!("Async task");
+                Task::Async(task) => {
+                    execute_async_task(task, self.pending_tasks.clone())
                 },
             }
         }
@@ -75,15 +75,15 @@ impl <T: VecStructure<Task>> Scheduler<T> {
     }
 }
 
-fn execute_sync_task<T: VecStructure<Task>>(task: Box<dyn FnOnce() -> Option<Task>>, pending_tasks: Arc<Mutex<T>>) {
-    if let Some(next_task) = task() {
+fn execute_sync_task<T: VecStructure<Task>>(task: Box<dyn SyncTask>, pending_tasks: Arc<Mutex<T>>) {
+    if let Some(next_task) = task.execute() {
         pending_tasks.lock().push(&next_task);
     }
 }
 
-fn execute_async_task2<T: 'static + VecStructure<Task>>(task: Box<dyn FnOnce() -> Pin<Box<dyn std::future::Future<Output = Option<Task>>>>>, pending_tasks: Arc<Mutex<T>>) {
+fn execute_async_task<T: 'static + VecStructure<Task>>(task: Box<dyn AsyncTask>, pending_tasks: Arc<Mutex<T>>) {
     ic_cdk::spawn(async move {
-        if let Some(next_task) = task().await {
+        if let Some(next_task) = task.execute().await {
             pending_tasks.lock().push(&next_task);
         }
     })
