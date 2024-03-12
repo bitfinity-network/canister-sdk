7869e3a8b90cd994e98cffe4e5b8aeccedf22ef4
Francesco <ufoscout@gmail.com>
2024-03-12 16:17:25 +0100

    Revert "Auxiliary commit to revert individual files from cd1b20dac5a05dd22cdbe214826bb879df8e01d1"
    This reverts commit 98ed4d82b198cc024d958f38f16d86a261e3b95e.

--- ic-stable-structures/tests/pocket_ic_tests/btreemap.rs
+++ ic-stable-structures/tests/pocket_ic_tests/btreemap.rs
@@ -2,7 +2,7 @@ use super::with_pocket_ic_context;
 
 #[test]
 fn should_init_tx_btreemap() {
-    with_pocket_ic_context(|_, ctx| {
+    with_pocket_ic_context(|ctx| {
         assert!(ctx.get_tx_from_btreemap(0)?.is_some());
 
         Ok(())
@@ -12,7 +12,7 @@ fn should_init_tx_btreemap() {
 
 #[test]
 fn should_push_tx_to_btreemap() {
-    with_pocket_ic_context(|_, ctx| {
+    with_pocket_ic_context(|ctx| {
         ctx.insert_tx_to_btreemap(1, 1, 10)?;
 
         assert!(ctx.get_tx_from_btreemap(1).unwrap().is_some());
@@ -24,7 +24,7 @@ fn should_push_tx_to_btreemap() {
 
 #[test]
 fn should_persist_btreemap_tx_after_upgrade() {
-    with_pocket_ic_context(|_, ctx| {
+    with_pocket_ic_context(|ctx| {
         ctx.insert_tx_to_btreemap(1, 1, 10)?;
 
         assert!(ctx.get_tx_from_btreemap(1)?.is_some());
--- ic-stable-structures/tests/pocket_ic_tests/cached_btreemap.rs
+++ ic-stable-structures/tests/pocket_ic_tests/cached_btreemap.rs
@@ -2,7 +2,7 @@ use super::with_pocket_ic_context;
 
 #[test]
 fn should_init_tx_cached_btreemap() {
-    with_pocket_ic_context(|_, ctx| {
+    with_pocket_ic_context(|ctx| {
         assert!(ctx.get_tx_from_cached_btreemap(0)?.is_some());
 
         Ok(())
@@ -12,7 +12,7 @@ fn should_init_tx_cached_btreemap() {
 
 #[test]
 fn should_push_tx_to_cached_btreemap() {
-    with_pocket_ic_context(|_, ctx| {
+    with_pocket_ic_context(|ctx| {
         // We saturate the cache to force eviction
         for i in 1..100 {
             ctx.insert_tx_to_cached_btreemap(i, i, 10 + i)?;
@@ -30,7 +30,7 @@ fn should_push_tx_to_cached_btreemap() {
 
 #[test]
 fn should_persist_cached_btreemap_tx_after_upgrade() {
-    with_pocket_ic_context(|_, ctx| {
+    with_pocket_ic_context(|ctx| {
         ctx.insert_tx_to_cached_btreemap(1, 1, 10)?;
 
         assert!(ctx.get_tx_from_cached_btreemap(1)?.is_some());
