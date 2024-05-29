71766e88ba33c82c452ce4222e0ae49ff31ae9dd
Francesco <ufoscout@gmail.com>
2024-05-29 10:16:29 +0200

    fmt

--- ic-stable-structures/src/structure/heap/mod.rs
+++ ic-stable-structures/src/structure/heap/mod.rs
@@ -2,12 +2,10 @@ mod btreemap;
 mod cell;
 mod log;
 mod multimap;
-// mod unbounded;
 mod vec;
 
 pub use btreemap::HeapBTreeMap;
 pub use cell::HeapCell;
 pub use log::HeapLog;
 pub use multimap::{HeapMultimap, HeapMultimapIter};
-// pub use unbounded::{HeapUnboundedIter, HeapUnboundedMap};
 pub use vec::HeapVec;
--- ic-stable-structures/src/structure/stable_storage/mod.rs
+++ ic-stable-structures/src/structure/stable_storage/mod.rs
@@ -2,12 +2,10 @@ mod btreemap;
 mod cell;
 mod log;
 mod multimap;
-// mod unbounded;
 mod vec;
 
 pub use btreemap::StableBTreeMap;
 pub use cell::StableCell;
 pub use log::StableLog;
 pub use multimap::{StableMultimap, StableMultimapIter, StableMultimapRangeIter};
-// pub use unbounded::{StableUnboundedIter, StableUnboundedMap};
 pub use vec::StableVec;
