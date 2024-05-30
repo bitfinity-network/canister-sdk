ff20f4687ba7fd9de81188b74c8c74dc916c07b3
f3kilo <f3kilo@yandex.ru>
2024-05-30 11:29:46 +0300

    bounded principal

--- ic-stable-structures/src/structure/common/mod.rs
+++ ic-stable-structures/src/structure/common/mod.rs
@@ -1,5 +1,6 @@
 pub mod ring_buffer;
 
+use candid::Principal;
 pub use ring_buffer::{StableRingBuffer, StableRingBufferIndices};
 
 /// A trait for types that have a minimum and maximum value.
@@ -81,4 +82,36 @@ impl Bounded for f64 {
 impl<const N: usize> Bounded for [u8; N] {
     const MIN: [u8; N] = [u8::MIN; N];
     const MAX: [u8; N] = [u8::MAX; N];
+}
+
+/// Principal sorted by two fields:
+/// 1) `len: u8` -> min = 0, max = 29;
+/// 2) `bytes: [u8; Self::MAX_LENGTH_IN_BYTES]` -> min = [], max = [0xFF; 29];
+impl Bounded for Principal {
+    const MIN: Self = Principal::from_slice(&[]); // Management canister principal;
+    const MAX: Self = Principal::from_slice(&[0xFF; 29]);
+}
+
+#[cfg(test)]
+mod tests {
+    use candid::Principal;
+
+    use crate::Bounded;
+
+    #[test]
+    fn correct_principal_bounds() {
+        let min_principal = Principal::MIN;
+        let max_principal = Principal::MAX;
+
+        let mut some_principals = vec![Principal::anonymous(), Principal::management_canister()];
+
+        let other_principals_iter =
+            (1..30).map(|i| Principal::from_slice(&vec![i as u8; i as usize]));
+        some_principals.extend(other_principals_iter);
+
+        for principal in some_principals {
+            assert!(principal >= min_principal);
+            assert!(principal <= max_principal);
+        }
+    }
 }
